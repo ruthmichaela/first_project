de100658a0c4b59edded3d697d86a64d22ff68b5
Ruth Herdman <ruthmichaela@gmail.com>
2017-02-23 12:59:00 -0500

    Migrations and Django Admin Setup

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+# import your model
+from collection.models import Users
+
+class UsersAdmin(admin.ModelAdmin):
+	model = Users
+	list_display = ('name', 'description',)
+	prepopulated_fields = {'slug': ('name',)}
+
+	# and register it
+admin.site.register(Users, UsersAdmin)
--- collection/models.py
+++ collection/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Users(models.Model):
+	name = models.CharField(max_length=255)
+	description = models.TextField()
+	slug = models.SlugField(unique=True)
+
 # Create your models here.
