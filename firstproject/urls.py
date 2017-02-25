1dd7eb470fc51b9847be4f3834b62233e3170738
Ruth Herdman <ruthmichaela@gmail.com>
2017-02-25 16:52:23 -0500

    User registration setup

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from collection import views
@@ -9,5 +9,6 @@ urlpatterns = [
 	url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
 	url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail, name='thing_detail'),
 	url(r'^things/(?P<slug>[-\w]+)/edit/$', views.edit_thing, name='edit_thing'),
+	url(r'^accounts/', include('registration.backends.simple.urls')),
 	url(r'^admin/', admin.site.urls),
 ]
