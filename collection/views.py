50f8a93f815ac3830e2809533e43b3906a7dfff7
Ruth Herdman <ruthmichaela@gmail.com>
2017-02-24 22:45:30 -0500

    forms.py - object editing capabilities on site

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from collection.forms import ThingForm
+
 from collection.models import Thing
 
 def index(request):
@@ -7,9 +9,25 @@ def index(request):
 	return render(request, 'index.html', {
 		'things': things,
 	})
+
 def thing_detail(request, slug):
 		thing = Thing.objects.get(slug=slug)
 
 		return render(request, 'things/thing_detail.html', {
 			'thing': thing,
+		})
+
+def edit_thing(request, slug):
+		thing = Thing.objects.get(slug=slug)
+		form_class = ThingForm
+		if request.method == 'POST':
+			form = form_class(data=request.POST, instance=thing)
+			if form.is_valid():
+				form.save()
+				return redirect('thing_detail', slug=thing.slug)
+		else:
+			 form = form_class(instance=thing)
+
+		return render(request, 'things/edit_thing.html', {
+			'thing': thing, 'form': form,
 		})
